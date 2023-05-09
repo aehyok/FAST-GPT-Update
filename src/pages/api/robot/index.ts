f27e9ed385fd3c1a5b4b8f6e6fb79dfc3cd7f78e
aehyok <455043818@qq.com>
2023-05-10 07:22:57 +0800

    feat：引入qdrant

---
+++ src/pages/api/robot/index.ts
@@ -0,0 +1,29 @@
+/*
+ * @Author: 刘启明 455043818@qq.com
+ * @Date: 2023-04-17 15:58:32
+ * @LastEditors: aehyok 455043818@qq.com
+ * @LastEditTime: 2023-05-09 23:31:22
+ * @FilePath: \AK47-GPT\src\pages\api\embeddings.ts
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import type { NextApiRequest, NextApiResponse } from "next";
+import {QdrantClient} from '@qdrant/js-client-rest';
+
+// TO connect to Qdrant running locally
+
+// or connect to Qdrant Cloud
+const client = new QdrantClient({
+    url: process.env.QDRANT_URL,
+    apiKey: process.env.QDRANT_API_KEY,
+});
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log("get", req.url);
+  const result = await client.getCollections();
+  console.log('List of collections:', result.collections);
+  res.status(200).json({ name: result });
+}
